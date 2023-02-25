7e2a94d4111f33fb9962b62f2c30a0942a34ecdd
geoHeil <e1225063@student.tuwien.ac.at>
2023-02-25 13:05:27 +0100

    Update handlers.py

--- camelot/handlers.py
+++ camelot/handlers.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from contextlib import contextmanager
-import io
 import os
 import sys
 
-from PyPDF2 import PdfFileReader, PdfFileWriter
+from pypdf import PdfReader, PdfWriter
 
 from .core import TableList
 from .parsers import Stream, Lattice
 from .utils import (
-    InvalidArguments,
     TemporaryDirectory,
     get_page_layout,
     get_text_objects,
     get_rotation,
     is_url,
-    get_url_bytes,
+    download_url,
 )
 
 
@@ -27,33 +24,19 @@ class PDFHandler(object):
 
     Parameters
     ----------
-    filepath : str | pathlib.Path, optional (default: None)
-        Filepath or URL of the PDF file. Required if file_bytes is not given
+    filepath : str
+        Filepath or URL of the PDF file.
     pages : str, optional (default: '1')
         Comma-separated page numbers.
         Example: '1,3,4' or '1,4-end' or 'all'.
     password : str, optional (default: None)
         Password for decryption.
-    file_bytes : io.IOBase, optional (default: None)
-        A file-like stream. Required if filepath is not given
 
     """
 
-    def __init__(self, filepath=None, pages="1", password=None, file_bytes=None):
+    def __init__(self, filepath, pages="1", password=None):
         if is_url(filepath):
-            file_bytes = get_url_bytes(filepath)
-
-        if not filepath and not file_bytes:
-            raise InvalidArguments('Either `filepath` or `file_bytes` is required')
-        if not filepath:
-            # filepath must either be passed, or taken from the name attribute
-            filepath = getattr(file_bytes, 'name')
-            if not filepath:
-                msg = ('Either pass a `filepath`, or give the '
-                       '`file_bytes` argument a name attribute')
-                raise InvalidArguments(msg)
-        self.file_bytes = file_bytes  # ok to be None
-
+            filepath = download_url(filepath)
         self.filepath = filepath
         if not filepath.lower().endswith(".pdf"):
             raise NotImplementedError("File format not supported")
@@ -65,28 +48,6 @@ class PDFHandler(object):
             if sys.version_info[0] < 3:
                 self.password = self.password.encode("ascii")
         self.pages = self._get_pages(pages)
-
-    @contextmanager
-    def managed_file_context(self):
-        """Reads from either the `filepath` or `file_bytes`
-        attribute of this instance, to return a file-like object.
-        Closes any open file handles on exit or error.
-
-        Returns
-        -------
-        file_bytes : io.IOBase
-            A readable, seekable, file-like object
-        """
-        if self.file_bytes:
-            # if we can't seek, write to a BytesIO object that can,
-            # then seek to the beginning before yielding
-            if not hasattr(self.file_bytes, 'seek'):
-                self.file_bytes = io.BytesIO(self.file_bytes.read())
-            self.file_bytes.seek(0)
-            yield self.file_bytes
-        else:
-            with open(self.filepath, "rb") as file_bytes:
-                yield file_bytes
 
     def _get_pages(self, pages):
         """Converts pages string to list of ints.
@@ -110,20 +71,20 @@ class PDFHandler(object):
         if pages == "1":
             page_numbers.append({"start": 1, "end": 1})
         else:
-            with self.managed_file_context() as f:
-                infile = PdfFileReader(f, strict=False)
+            with open(self.filepath, "rb") as f:
+                infile = PdfReader(f, strict=False)
 
-                if infile.isEncrypted:
+                if infile.is_encrypted:
                     infile.decrypt(self.password)
 
                 if pages == "all":
-                    page_numbers.append({"start": 1, "end": infile.getNumPages()})
+                    page_numbers.append({"start": 1, "end": len(infile.pages)})
                 else:
                     for r in pages.split(","):
                         if "-" in r:
                             a, b = r.split("-")
                             if b == "end":
-                                b = infile.getNumPages()
+                                b = len(infile.pages)
                             page_numbers.append({"start": int(a), "end": int(b)})
                         else:
                             page_numbers.append({"start": int(r), "end": int(r)})
@@ -146,15 +107,15 @@ class PDFHandler(object):
             Tmp directory.
 
         """
-        with self.managed_file_context() as fileobj:
-            infile = PdfFileReader(fileobj, strict=False)
-            if infile.isEncrypted:
+        with open(filepath, "rb") as fileobj:
+            infile = PdfReader(fileobj, strict=False)
+            if infile.is_encrypted:
                 infile.decrypt(self.password)
             fpath = os.path.join(temp, f"page-{page}.pdf")
             froot, fext = os.path.splitext(fpath)
-            p = infile.getPage(page - 1)
-            outfile = PdfFileWriter()
-            outfile.addPage(p)
+            p = infile.pages[page - 1]
+            outfile = PdfWriter()
+            outfile.add_page(p)
             with open(fpath, "wb") as f:
                 outfile.write(f)
             layout, dim = get_page_layout(fpath)
@@ -167,16 +128,16 @@ class PDFHandler(object):
                 fpath_new = "".join([froot.replace("page", "p"), "_rotated", fext])
                 os.rename(fpath, fpath_new)
                 instream = open(fpath_new, "rb")
-                infile = PdfFileReader(instream, strict=False)
-                if infile.isEncrypted:
+                infile = PdfReader(instream, strict=False)
+                if infile.is_encrypted:
                     infile.decrypt(self.password)
-                outfile = PdfFileWriter()
-                p = infile.getPage(0)
+                outfile = PdfWriter()
+                p = infile.pages[0]
                 if rotation == "anticlockwise":
-                    p.rotateClockwise(90)
+                    p.rotate(90)
                 elif rotation == "clockwise":
-                    p.rotateCounterClockwise(90)
-                outfile.addPage(p)
+                    p.rotate(-90)
+                outfile.add_page(p)
                 with open(fpath, "wb") as f:
                     outfile.write(f)
                 instream.close()
